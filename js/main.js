db46dfaa12228e7b8a6c37fa145d1a594ab74974
limtaegeun <imori333@gmail.com>
2020-11-16 12:32:56 +0900

    add length and amp to Wave

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ function init() {
 }
 
 let timing = 0
-let timing2 = 800
 
 function draw() {
     const canvas = document.getElementById('tutorial')
@@ -12,14 +11,13 @@ function draw() {
         if (timing < -360) {
             timing = 0
         }
-        if (timing2 < -360) {
-            timing2 = 0
-        }
         let ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, 400, 400); // 캔버스를 비운다
-        timing2 = timing2 - 1.3
-        sinCurv(ctx, timing--, 'rgba(130,3,40, 0.3)')
-        sinCurv(ctx, timing2, 'rgba(13,34,240, 0.3)')
+        ctx.clearRect(0, 0, 800, 400); // 캔버스를 비운다
+        timing = timing - 0.5
+
+        sinCurv(ctx, timing, 'rgba(255,0,0,0.4', 60, 30)
+        sinCurv(ctx, timing, 'rgba(0,255,0,0.4', 30, 40)
+        sinCurv(ctx, timing, 'rgba(0,0,255,0.4', 45, 50)
         window.requestAnimationFrame(draw)
 
     }
@@ -27,23 +25,24 @@ function draw() {
 
 
 
-function sinCurv(ctx, rad, color) {
+function sinCurv(ctx, rad, color, length, amp) {
     let curv = new Path2D()
-    curv.fillStyle = color
     curv.moveTo(0, 200)
     let prevX = 0
     let prevY = 200
-    for (let i = 1; i < 100; i++) {
+    for (let i = -3; i < 100; i++) {
         let curX = 10 * i
-        let curY = Math.sin(Math.PI / 4 * i + (Math.PI / 18 * rad)) * 30 + 200
+        let curY = Math.sin(Math.PI / length * i + (Math.PI / 18 * rad)) * amp + 200
             // ctx.lineTo((curX + prevX) / 2, (prevY + curY) / 2)
         curv.quadraticCurveTo(prevX, prevY, (curX + prevX) / 2, (prevY + curY) / 2)
 
         prevX = curX
         prevY = curY
     }
-    curv.lineTo(400, 400)
+
+    curv.lineTo(800, 400)
     curv.lineTo(0, 400)
+    ctx.fillStyle = color
     ctx.fill(curv)
 }
 
